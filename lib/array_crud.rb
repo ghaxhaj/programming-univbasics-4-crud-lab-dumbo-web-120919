9e70634e759d24915c8872d5316ab583897f5198
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-12-01 01:09:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    add_element_to_end_of_array<< "element"
+    create_an_array << "mooses"
 end
 
 def add_element_to_start_of_array(array, element)
